75ca6558a62b5e90c27ced31ce434b4534eb5ca0
Israel Booz <A00840268@tec.mx>
2025-11-24 19:03:38 -0600

    Minijuegos Restructura

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.navigation:navigation-compose:2.7.5")
-
+    //Holap
     // Para cargar imágenes desde URLs
     implementation("io.coil-kt:coil-compose:2.5.0")
 
